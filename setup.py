77fa96be2326c7e1fd4c757bf96849ee0892b596
kevin <kevin@qudos.com>
2016-12-06 11:30:34 -0800

    Added versioning

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
+import os
+import re
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-VERSION = '99.4.18'
+version = re.compile(r'__version__\s*=\s*[\'\"](.+)[\"\']')
+
+def get_package_version():
+    "returns package version without importing it"
+    base = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(base, "tcelery/__init__.py")) as initf:
+        for line in initf:
+            m = version.match(line.strip())
+            if not m:
+                continue
+            return m.groups(1)[0]
 
 setup(name='tornado-redis',
-      version=VERSION,
+      version=get_package_version(),
       description='Asynchronous Redis client for the Tornado Web Server.',
       author='Vlad Glushchuk',
       author_email='vgluschuk@gmail.com',
--- tornadoredis/__init__.py
+++ tornadoredis/__init__.py
@@ -2,3 +2,6 @@ from .client import Connection, Client
 from .exceptions import (RedisError, ConnectionError, ResponseError,
                          InvalidResponse)
 from .connection import ConnectionPool
+
+__version__ = '99.4.18'
+VERSION = tuple(int(s) for s in __version__.split('.'))
