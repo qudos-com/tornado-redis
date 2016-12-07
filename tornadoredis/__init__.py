1e3a8d8c216fef447f753edf1e0d2228d2e5cc80
Qubot <admin@qudos.com>
2016-12-07 11:31:47 -0800

    Bump version: 99.4.20 → 99.4.21

--- tornadoredis/__init__.py
+++ tornadoredis/__init__.py
@@ -3,5 +3,5 @@ from .exceptions import (RedisError, ConnectionError, ResponseError,
                          InvalidResponse)
 from .connection import ConnectionPool
 
-__version__ = '99.4.20'
+__version__ = '99.4.21'
 VERSION = tuple(int(s) for s in __version__.split('.'))
